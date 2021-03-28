e8fc5610bed42011720c7ce2b2e0bc6e0fd83c57
arif fadhillah <ariffadhillah319@gmail.com>
2021-03-28 12:33:40 -0400

    setup file temple done

--- ecomerce/store/views.py
+++ ecomerce/store/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def store(request):
+    context = {}
+    return render(request, 'store/store.html', context)
+
+def card(request):
+    context = {}
+    return render(request, 'store/card.html', context)
+
+def checkout(request):
+    context = {}
+    return render(request, 'store/checkout.html', context)
+
